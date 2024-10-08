e8d641b2c525149532e798ccc27f215926ebc84f
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-08 16:48:48 +0200

    added compatible core version information

--- package.py
+++ package.py
@@ -4,5 +4,7 @@ version = "0.0.1-dev.1"
 
 client_dir = "ayon_jira"
 
-plugin_for = ["ayon_server"]
-build_command = ""
+ayon_required_addons = {
+    "core": ">=0.4.0",
+}
+ayon_compatible_addons = {}
